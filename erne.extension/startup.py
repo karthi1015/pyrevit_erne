8afce06bd950588c3d25782a698c260fa8752bc6
fbpyr <frederic.beaupere@erne.net>
2020-11-05 15:40:42 +0100

    add autoupdater

--- erne.extension/startup.py
+++ erne.extension/startup.py
@@ -1,6 +1,6 @@
-from rph import git
-
-git.pull_repo("pyrevit")
-git.pull_repo("pyRevit_erne")
+import git_autoupdate
 
 print("startup.py erne pyrevit auto updater..")
+
+git_autoupdate.pull_repo("pyrevit")
+git_autoupdate.pull_repo("pyRevit_erne")
