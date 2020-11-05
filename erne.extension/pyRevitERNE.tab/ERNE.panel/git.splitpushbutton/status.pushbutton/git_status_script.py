a25a7b5df2dfb22dd87df92acac1de807da8f26c
fbpyr <frederic.beaupere@erne.net>
2020-11-05 15:23:34 +0100

    add git

--- erne.extension/pyRevitERNE.tab/ERNE.panel/git.splitpushbutton/status.pushbutton/git_status_script.py
+++ erne.extension/pyRevitERNE.tab/ERNE.panel/git.splitpushbutton/status.pushbutton/git_status_script.py
@@ -1,20 +1,3 @@
-import os
-import clr
-clr.AddReference("LibGit2Sharp")
-from LibGit2Sharp import Repository, Commands
-from LibGit2Sharp import StatusOptions
+from rph import git
 
-
-def status_repo(name):
-    print("INFO: git status: {}".format(name))
-    target_path = os.path.join(PROG_DATA, name)
-    repo = Repository(str(target_path))
-    status = repo.RetrieveStatus(StatusOptions())
-    if status.IsDirty:
-        print("repo not clean - please contact your pyRevit admin!")
-        return
-    print("repo is clean: {}".format(not status.IsDirty))
-
-
-PROG_DATA = "C:\\ProgramData"
-status_repo("pyRevit_erne")
+git.status_repo("pyRevit_erne")
