54d58b69dd5548f1124f6ff125c422218d0b2176
fbpyr <frederic.beaupere@erne.net>
2020-11-06 09:23:35 +0100

    clean up

--- erne.extension/pyRevitERNE.tab/ERNE.panel/Inspect.pulldown/Elements_Of_Selected_Level.pushbutton/Elements_Of_Selected_Level_script.py
+++ erne.extension/pyRevitERNE.tab/ERNE.panel/Inspect.pulldown/Elements_Of_Selected_Level.pushbutton/Elements_Of_Selected_Level_script.py
@@ -1,11 +1,16 @@
 from Autodesk.Revit.DB import FilteredElementCollector as Fec
-from Autodesk.Revit.UI import TaskDialog
 from collections import defaultdict
 from rpw import doc, uidoc
+from pyrevit import forms
 
 
 def sep_line(length):
     return length * "_"
+
+def correct_selection():
+    if len(selection) == 1:
+        if "Level" in str(selection[0].GetType):
+            return True
 
 
 selection = [doc.GetElement(elId) for elId in uidoc.Selection.GetElementIds()]
@@ -13,31 +18,27 @@ all_elements = Fec(doc).WhereElementIsNotElementType().ToElements()
 element_categories = defaultdict(list)
 ws_table = doc.GetWorksetTable()
 
-if len(selection) == 1:
-    if "Level" in str(selection[0].GetType):
-        selected_level = selection[0]
-        counter = 0
-        print(selected_level.Name)
+if not correct_selection():
+    forms.alert('Exactly one level must be selected.', exitscript=True)
 
-        for i, element in enumerate(all_elements):
-            if element.LevelId == selected_level.Id:
-                counter += 1
-                element_categories[element.Category.Name].append(element)
+selected_level = selection[0]
+counter = 0
+print(selected_level.Name)
 
-        for cat in element_categories:
-            print("{}{}: {}".format(sep_line(15), cat, len(element_categories[cat])))
-            for elem in element_categories[cat]:
-                print("id: {} - workset: {}".format(elem.Id.IntegerValue, ws_table.GetWorkset(elem.WorksetId).Name))
+for i, element in enumerate(all_elements):
+    if element.LevelId == selected_level.Id:
+        counter += 1
+        element_categories[element.Category.Name].append(element)
 
-        print("{}{} Categories found in {}:".format(sep_line(15), len(element_categories), selected_level.Name))
+for cat in element_categories:
+    print("{}{}: {}".format(sep_line(15), cat, len(element_categories[cat])))
+    for elem in element_categories[cat]:
+        print("id: {} - workset: {}".format(elem.Id.IntegerValue, ws_table.GetWorkset(elem.WorksetId).Name))
 
-        for cat in element_categories:
-            print("{}: {}".format(cat, (len(element_categories[cat]))))
+print("{}{} Categories found in {}:".format(sep_line(15), len(element_categories), selected_level.Name))
 
-        print("{} Elements found in {}".format(counter, selected_level.Name))
-        print("{} Elements found in project.".format(all_elements.Count))
+for cat in element_categories:
+    print("{}: {}".format(cat, (len(element_categories[cat]))))
 
-    else:
-        TaskDialog.Show('pyRevit', 'Exactly one level must be selected.')
-else:
-    TaskDialog.Show('pyRevit', 'Exactly one level must be selected.')
+print("{} Elements found in {}".format(counter, selected_level.Name))
+print("{} Elements found in project.".format(all_elements.Count))
